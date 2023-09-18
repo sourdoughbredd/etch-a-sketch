c672e16e4b6657cd23d392625f14b80755d82bd0
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2023-09-18 15:27:08 -0400

    Format divs into grid using flex

---
+++ script.js
@@ -0,0 +1,32 @@
+const container = document.querySelector('.container');
+
+// Alter the DOM to build the grid structure with divs
+let grid_size = 16;
+for (let i = 0; i < grid_size; i++) {
+    const rowDiv = document.createElement('div');
+    rowDiv.classList.add('row');
+    for (let i = 0; i < grid_size; i++) {
+        const squareDiv = document.createElement('div');
+        squareDiv.classList.add('square');
+        rowDiv.appendChild(squareDiv);
+    }
+    container.appendChild(rowDiv);
+}
+
+// Format the grid using flexbox
+const rows = document.querySelectorAll('.row');
+const squares = document.querySelectorAll('.square');
+
+container.style.display = 'flex';
+container.style['flex-direction'] = 'column';
+rows.forEach(elem => {
+    elem.style.display = 'flex';
+    elem.style.flex = '1 1 auto';
+});
+
+// let widthStr = window.getComputedStyle(container).getPropertyValue('width');
+// let item_size =  parseFloat(widthStr) / grid_size;
+squares.forEach(elem => {
+    elem.style.flex = '1 1 auto';
+    elem.style.border = '2px solid black'
+});
